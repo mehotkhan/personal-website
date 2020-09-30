ad28f7b6f2ed6a17ebddf5030021ac6946bf3b3c
Ali Zemani <mehot1@gmail.com>
2020-09-30 10:30:18 +0330

    Site updated: 2020-09-30 10:30:17

--- sw.js
+++ sw.js
@@ -2,18 +2,40 @@
 
 'use strict';
 
-importScripts('/workbox-sw.min.js');
+importScripts('/assets/workbox-v5.1.4/workbox-sw.js');
 
-const { NetworkFirst, StaleWhileRevalidate, CacheFirst } = workbox.strategies;
-const { registerRoute } = workbox.routing;
-const { ExpirationPlugin } = workbox.expiration;
+workbox.setConfig({
+    modulePathPrefix: '/assets/workbox-v5.1.4/'
+});
+
+const {
+    NetworkFirst,
+    StaleWhileRevalidate,
+    CacheFirst
+} = workbox.strategies;
+const {
+    registerRoute
+} = workbox.routing;
+const {
+    ExpirationPlugin
+} = workbox.expiration;
 
 
 
-registerRoute('/', new NetworkFirst({"cacheName":"index","plugins":[]}), 'GET');
-registerRoute(/\.(?:js|css)$/, new StaleWhileRevalidate({"cacheName":"js-css","plugins":[]}), 'GET');
-registerRoute(/\.(?:png|gif|jpg|jpeg|svg)$/, new CacheFirst({"cacheName":"images","plugins":[new ExpirationPlugin({"maxEntries":60,"maxAgeSeconds":2592000})]}), 'GET');
+registerRoute('/', new NetworkFirst({
+    "cacheName": "index",
+    "plugins": []
+}), 'GET');
+registerRoute(/\.(?:js|css)$/, new StaleWhileRevalidate({
+    "cacheName": "js-css",
+    "plugins": []
+}), 'GET');
+registerRoute(/\.(?:png|gif|jpg|jpeg|svg)$/, new CacheFirst({
+    "cacheName": "images",
+    "plugins": [new ExpirationPlugin({
+        "maxEntries": 60,
+        "maxAgeSeconds": 2592000
+    })]
+}), 'GET');
 
 workbox.googleAnalytics.initialize();
-
-
