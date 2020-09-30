19e22c6a2fddf55316cdbea7b636d0a4335edd8b
Ali Zemani <mehot1@gmail.com>
2020-09-30 09:24:08 +0330

    Site updated: 2020-09-30 09:24:07

--- sw.js
+++ sw.js
@@ -14,6 +14,6 @@ registerRoute('/', new NetworkFirst({"cacheName":"index","plugins":[]}), 'GET');
 registerRoute(/\.(?:js|css)$/, new StaleWhileRevalidate({"cacheName":"js-css","plugins":[]}), 'GET');
 registerRoute(/\.(?:png|gif|jpg|jpeg|svg)$/, new CacheFirst({"cacheName":"images","plugins":[new ExpirationPlugin({"maxEntries":60,"maxAgeSeconds":2592000})]}), 'GET');
 
-
+workbox.googleAnalytics.initialize();
 
 
